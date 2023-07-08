c89ba12bc75f5df6129fc6567fbdb8b3be972fbf
MelihUnal60 <111892158+MelihUnal60@users.noreply.github.com>
2023-07-08 08:15:00 +0300

    Add project files.

---
+++ InvoiceAuditApp.ConsoleUI/Program.cs
@@ -0,0 +1,31 @@
+﻿using InvoiceAuditApp.Domain;
+
+Invoice invoice = new Invoice();
+invoice.DocDate = Convert.ToDateTime("02.11.2022");
+invoice.DocAmount = 342;
+invoice.SerialNumber = 11111;
+
+invoice.InvoiceDateUpdated += Invoice_InvoiceDateUpdated;
+invoice.DocDate = DateTime.Now;
+
+
+invoice.InvoiceSerialNumberUpdated += Invoice_InvoiceSerialNumberUpdated;
+invoice.SerialNumber = 12345;
+
+invoice.InvoiceAmountUpdated += Invoice_InvoiceAmountUpdated;
+invoice.DocAmount = 1345;
+
+void Invoice_InvoiceAmountUpdated(Invoice invoice, InvoiceAmountUpdateEventArgs e)
+{
+    Console.WriteLine($"{e.SerialNumber} numaralı faturanın belge tutarı değişti : {e.NewAmount}");
+}
+
+void Invoice_InvoiceSerialNumberUpdated(Invoice invoice, InvoiceSerialNumberUpdateEventArgs e)
+{
+    Console.WriteLine($"{e.OldSerialNumber} numaralı faturanın yeni belge numarası : {e.NewSerialNumber}");
+}
+
+void Invoice_InvoiceDateUpdated(Invoice invoice, InvoiceDateUpdateEventArgs e)
+{
+    Console.WriteLine($"{e.SerialNumber} numaralı faturanın tarihi güncellendi : {e.NewDate}");
+}
---
+++ InvoiceAuditApp.Domain/InvoiceDateUpdateEventHandler.cs
@@ -0,0 +1,12 @@
+﻿using InvoiceAuditApp.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvoiceAuditApp.Domain
+{
+    public delegate void InvoiceDateUpdateEventHandler(Invoice invoice, InvoiceDateUpdateEventArgs e);
+
+}
